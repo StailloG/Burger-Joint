c49b801485dd74dc50e1aa651535d44715274342
Stephanie Castillo <stephie.castles@gmail.com>
2022-11-14 22:56:19 -0500

    FIXED pickup & drop items

--- BurgerJoint/Assets/Scripts/Burger Scripts/BurgerAssembly.cs
+++ BurgerJoint/Assets/Scripts/Burger Scripts/BurgerAssembly.cs
@@ -4,31 +4,41 @@ using UnityEngine;
 
 public class BurgerAssembly : MonoBehaviour
 {
-    //variables
-    [Header("Gameobjects")]
+    /* Variables */
+    [Header("Trigger Check")]
     private bool canPickUp = false;
+    public int spacePressedAllowed = 0;
+
+    [Header("Gameobjects")]
     public GameObject playerHand;
     public GameObject ingredient;
 
+    [Header("Components")]
+    private Rigidbody rb;
 
-    /*
-     * Pick up & Drop item.
-     */
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    /* Pick up & drop item */
     void Update()
     {
-        if (canPickUp == true && Input.GetKeyDown(KeyCode.Space))
+        if (canPickUp == true && Input.GetKey(KeyCode.Space) && spacePressedAllowed == 0)
         {
             AttachToParent();
+            spacePressedAllowed = 1;
+            Debug.Log("Picked up an ingredient.");
         }
-        if (canPickUp == false && Input.GetKeyDown(KeyCode.E))
+        if (canPickUp == false && Input.GetKey(KeyCode.E) && spacePressedAllowed == 1)
         {
             DetachFromParent();
+            spacePressedAllowed = 0;
+            Debug.Log("Dropped an ingredient.");
         }
     }
 
-    /*
-     * If player collides with ingredients
-     */
+    /* If player's hand collides with item */
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Hand")
@@ -42,23 +52,23 @@ public class BurgerAssembly : MonoBehaviour
         canPickUp = false;
     }
 
-    /*
-     * Player is the parent & ingredients are the child.
-     */
+    /* Player is the parent & item is the child */
     public void AttachToParent()
     {
         //burger is now child of player's hand
         ingredient.transform.parent = playerHand.transform;
 
-        //set the pos of burger in front of player at all times
-        transform.localPosition = new Vector3(0.4001f, -1.899f, -0.999f);
-
+        //gravity doesn't take place in player's hand
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+        
         canPickUp = false;
     }
 
+    /* Drop item */
     public void DetachFromParent()
     {
         transform.parent = null;
-        
+
+        rb.constraints = RigidbodyConstraints.None;
     }
 }
