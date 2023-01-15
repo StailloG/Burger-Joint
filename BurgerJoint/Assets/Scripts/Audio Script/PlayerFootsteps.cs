d1375d6dff4a003376691a47496399bf18e48884
PetersonNormil <petersonnormil@gmail.com>
2023-01-15 18:14:00 -0500

    beginning set up for audio footsteps

--- BurgerJoint/Assets/Scripts/Audio Script/PlayerFootsteps.cs
+++ BurgerJoint/Assets/Scripts/Audio Script/PlayerFootsteps.cs
@@ -8,7 +8,10 @@ public class PlayerFootsteps : MonoBehaviour
 {
     private AudioSource audioSource;
     public List<AudioClip> tileFootsteps;
-
+    public List<AudioClip> carpetFootsteps;
+    public List<AudioClip> grassFootsteps;
+    [SerializeField] private FootstepType currentFootstepType;
+    
     [Header("Random Params")] 
     [SerializeField] [Range(0.01f, 0.99f)] private float minVolume = 0.8f;
     [SerializeField] [Range(0.02f, 1f)] private float maxVolume = 1f;
@@ -35,11 +38,43 @@ public class PlayerFootsteps : MonoBehaviour
         timer = 0;
     }
 
-    public void PlayFootstepAudio()
+    private void PlayFootstepAudio()
     {
-        var clip = tileFootsteps[Random.Range(0, tileFootsteps.Count)];
+        var clip = DetermineFootstepClip();
+       
+        
         audioSource.volume = Random.Range(minVolume, maxVolume);
         audioSource.pitch = Random.Range(0.9f, 1.1f);
         audioSource.PlayOneShot(clip);
     }
+
+    private AudioClip DetermineFootstepClip()
+    {
+        switch (currentFootstepType)
+        {
+            case FootstepType.TILE:
+                return tileFootsteps[Random.Range(0, tileFootsteps.Count)];
+                break;
+            case FootstepType.GRASS:
+                return grassFootsteps[Random.Range(0, grassFootsteps.Count)];
+                break;
+            case FootstepType.CARPET:
+                return carpetFootsteps[Random.Range(0, carpetFootsteps.Count)];
+                break;
+            default:
+                Debug.Log("Default footstep being called");
+                return tileFootsteps[Random.Range(0, tileFootsteps.Count)];
+                break;
+            
+        }
+    }
+}
+
+
+public enum FootstepType
+{
+    NONE,
+    CARPET,
+    TILE,
+    GRASS
 }
