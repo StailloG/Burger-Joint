33f0d0b697033ad144ebecf93e707d61516b3bf7
Stephanie Castillo <stephie.castles@gmail.com>
2023-01-15 19:37:14 -0500

    Working on raycast picking up ingredients

--- BurgerJoint/Assets/Scripts/CameraScripts/Raycast.cs
+++ BurgerJoint/Assets/Scripts/CameraScripts/Raycast.cs
@@ -17,11 +17,24 @@ public class Raycast : MonoBehaviour
         {
             //Debug.Log("Hit something");
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitinfo.distance, Color.green);
+
+            //identifies ingredient
+            PickUp(hitinfo, "Tomato");
+            
         }
         else
         {
             //Debug.Log("Nothing has been hit");
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 20f,  Color.red);
+        }
+    }
+
+    private void PickUp(RaycastHit ray, string name)
+    {
+        if (ray.collider.CompareTag(name))
+        {
+            Debug.Log("A " + name + " has been hit!");
+            //GameObject itemGrabbed = ray.collider.gameObject;
         }
     }
 }
