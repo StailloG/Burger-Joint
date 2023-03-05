f3b823bc1720bb8516829f3c97ee5515c5b17bb5
PetersonNormil <petersonnormil@gmail.com>
2023-03-04 22:15:04 -0500

    pick up and dropping items featyre

--- BurgerJoint/Assets/Scripts/Counter Scripts/ClearCounter.cs
+++ BurgerJoint/Assets/Scripts/Counter Scripts/ClearCounter.cs
@@ -8,7 +8,34 @@ public class ClearCounter : BaseCounter
  
   public override void Interact(PlayerMovement player)
   {
-
+    if(!HasKitchenObject())
+    {
+      //^there is no kitchen object on the counter
+      if (player.HasKitchenObject())
+      {
+        //drop object that player has and put it on the coujnter
+        player.GetKitchenObject().SetKitchenObjectParent(this);
+      }
+      else
+      {
+        //if player doesnt have a kitchen object on hand
+      }
+    }
+    else
+    {
+      //there is already a kitchen object on the counter
+      if (player.HasKitchenObject())
+      {
+        
+      }
+      else
+      {
+        //if player does not have a kitchen object and there is a kitchen obkect on the counter 
+        //place the the object on the counter to the player
+        
+        GetKitchenObject().SetKitchenObjectParent(player);
+      }
+    }
     
   }
 
--- BurgerJoint/Assets/Scripts/Counter Scripts/ContainerCounter.cs
+++ BurgerJoint/Assets/Scripts/Counter Scripts/ContainerCounter.cs
@@ -12,9 +12,12 @@ public class ContainerCounter : BaseCounter
     
     public override void Interact(PlayerMovement player)
     {
+        if (!player.HasKitchenObject())
+        {
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSo.prefab);
             kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
             OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
     }
 
     
