411ddcab8201f0e8b4baf210b317669cbed9794c
PetersonNormil <petersonnormil@gmail.com>
2023-03-11 20:45:54 -0500

    continuuing music systme

--- BurgerJoint/Assets/Scenes/Test Scene/MusicLayerSystem.cs
+++ BurgerJoint/Assets/Scenes/Test Scene/MusicLayerSystem.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.PlayerLoop;
 
 /// <summary>
@@ -10,12 +11,15 @@ using UnityEngine.PlayerLoop;
 public class MusicLayerSystem : MonoBehaviour
 {
   [SerializeField] private AudioClip[] audioStems;
-  [SerializeField] private float fadeInTime = 1.5f;
- // [SerializeField] private float fadeOutTime = 1.5f;
+  
+  [SerializeField][Range(0.01f, 12f)] private float fadeInTime = 1.5f;
+  [SerializeField][Range(0.01f, 12f)] private float fadeOutTime = 1.5f;
+  //set your audiosources to one music group
+  public AudioMixerGroup musicMixerGroup; 
   
   private AudioSource[] audioSources;
   
-//  private int currentIntensity = 0;//currentIntensity = 0 is the first music layer playing  
+  private int currentIntensity = 0;//currentIntensity = 0 is the first music layer playing  
   
   private int amountOfStems;
   private float musicVolumeMax = 1f;
@@ -47,7 +51,9 @@ public class MusicLayerSystem : MonoBehaviour
 
       //assign clip to the audioSource
       newAudioSource.clip = audioStems[i];
-    
+
+      newAudioSource.outputAudioMixerGroup = musicMixerGroup;
+      
       //add to the audioSources array
       audioSources[i] = newAudioSource;
     }
@@ -56,6 +62,7 @@ public class MusicLayerSystem : MonoBehaviour
   //start 
   public void StartMusicSystem()
   {
+
     audioSources[0].Play();
     //fade in first audiosource
     StartCoroutine(FadeInMusicStem(audioSources[0],fadeInTime));
@@ -65,15 +72,43 @@ public class MusicLayerSystem : MonoBehaviour
       audioSources[i].Play();
       audioSources[i].mute = true;
     }
+
+    currentIntensity = 0;
   }
   
   //stop 
   
   
   //increase intensity 
+  public void IncreaseIntensity()
+  {
+    if (currentIntensity >= amountOfStems - 1)
+    {
+      Debug.LogWarning("MusicLayerSystem: Already reached highest intensity");
+      return;
+    }
+
+    currentIntensity++;
+
+    StartCoroutine(FadeInMusicStem(audioSources[currentIntensity], fadeInTime));
+    
+  }
+  
   
   //decrease intensity 
-  
+  public void DecreaseIntensity()
+  {
+    if (currentIntensity <= 0)
+    {
+      Debug.LogWarning("MusicLayerSystem: Already reached lowest intensity");
+      return;
+    }
+    StartCoroutine(FadeOutMusicStem(audioSources[currentIntensity], fadeOutTime));
+    currentIntensity--;
+
+   
+    
+  }
   
   
   //fade in 
@@ -93,5 +128,17 @@ public class MusicLayerSystem : MonoBehaviour
 
   }
   //fade out 
-  
+  private IEnumerator FadeOutMusicStem(AudioSource activeSource, float transitionTime)
+  {
+
+    for (float t = 0.0f; t <= transitionTime; t += Time.deltaTime)
+    {
+      activeSource.volume = (musicVolumeMax -((t / transitionTime) * musicVolumeMax));
+      yield return null;
+    }
+    activeSource.mute = true;
+
+    activeSource.volume = musicVolumeMax; 
+
+  }
 }
