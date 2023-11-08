29e4848a7f6607e1ff8844371974931de36be78f
Stephanie Castillo <stephie.castles@gmail.com>
2023-11-07 20:31:32 -0500

    dialogue scripts

--- BurgerJoint/Assets/Scripts/Dialogue/DialogueManager.cs
+++ BurgerJoint/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
+    [Header("Text")]
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+
     private Queue<string> sentences;
 
     void Start()
@@ -13,6 +19,35 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " + dialogue.name);
+        //Debug.Log("Starting conversation with " + dialogue.name);
+        nameText.text = dialogue.name;
+
+        sentences.Clear(); //clear prev convo
+
+        foreach (string sentence in dialogue.sentences) //(string item in collection)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0) //if reached end of convo/Queue
+        {
+            EndDialogue();
+            return; //return out of the rest of the function
+        }
+
+        //if there's still convo left to say
+        string sentence = sentences.Dequeue();
+        //Debug.Log(sentence);
+        dialogueText.text = sentence;
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("End of conversation.");
     }
 }
